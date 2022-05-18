571e6eab9f83f93583a583b90eca279834fec585
pengcheng-hou <hphilip168@gmail.com>
2022-05-17 21:37:04 -0400

    ajax day2

--- 原生ajax/server.js
+++ 原生ajax/server.js
@@ -12,6 +12,35 @@ app.get('/server', (request, response) => {
 
 
 })
+app.post('/server', (request, response) => {
+        // 设置一个响应头，设置允许跨域
+        response.setHeader("Access-Control-Allow-Origin", "*")
+            // 设置一个响应：
+        response.send('hello json hphilip')
+    })
+    // 设置所有请求都可以接受处理
+    // app.all('/server', (request, response) => {
+    //     // 设置一个响应头，设置允许跨域
+    //     response.setHeader("Access-Control-Allow-Origin", "*")
+    //         // 设置接受任何请求头信息
+    //     response.setHeader('Access-Control-Allow-Headers', "*")
+    //         // 设置一个响应：
+    //     response.send('hello AJAX POST')
+
+
+// })
+app.all('/json-server', (request, response) => {
+    // 设置一个响应头，设置允许跨域
+    response.setHeader("Access-Control-Allow-Origin", "*")
+    response.setHeader("Access-Control-Allow-Headers", "*")
+        // 设置一个响应数据对象：
+    const student = { name: "philip", age: 35, Gender: "Male" }
+        // 对响应数据对象做一个转换：进行字符串转换
+    let str = JSON.stringify(student)
+        //  response.send 传递的参数只能是字符串，所以要进行json转换
+    response.send(str)
+
+})
 
 //4.监听端口启动服务
 app.listen(8000, () => { console.log('server is running at port 8000....') })
